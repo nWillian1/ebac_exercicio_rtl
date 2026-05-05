aa5b28c4b4cb7c29188afa64b40eabb4c11a49d9
Willian Nogueira <willian.nogueira2016@hotmail.com>
2026-05-05 15:48:47 -0300

    Finaliza os testes com Jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Post from "./components/Post";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
-        Olha só que legal minha miniatura do Batmóvel.
+      <Post imageUrl="https://m.media-amazon.com/images/I/71tqJslVD5L.jpg">
+        Olha só que legal minha miniatura do Batmóvel modelo 1966.
       </Post>
     </div>
   );
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+describe('Teste para o componente PostComments', () => {
+    test('Deve renderizar o componente corretamente', () => {
+        render(<Post />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve permitir a inserção de dois comentários', () => {
+        render(<Post />);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: { value: 'Primeiro comentário de teste' }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: { value: 'Segundo comentário de teste' }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getByText('Primeiro comentário de teste')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário de teste')).toBeInTheDocument();
     });
 });
